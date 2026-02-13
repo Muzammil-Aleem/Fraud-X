5a294775120eadaacd9564b1b932af2f242f97db
Muzammil-Aleem <157204550+Muzammil-Aleem@users.noreply.github.com>
2026-02-13 18:53:13 +0500

    Add files via upload

--- train_fraud.py
+++ train_fraud.py
@@ -14,7 +14,7 @@ y = data["Class"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Train model
-model = RandomForestClassifier(n_estimators=50)
+model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
 # Save model
